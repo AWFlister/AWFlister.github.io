e7c88ac197e72f477305f335410957674d12fe0a
Aryasakti Wirasena <aryasaktiwirasena@gmail.com>
2022-06-04 14:03:47 +0700

    Updates

--- _nuxt/static/1654325633/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fawflister.github.io\u002F_nuxt\u002Fstatic\u002F1654325633",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fawflister.github.io\u002F",assetsPath:"\u002Fawflister.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1654326224/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fawflister.github.io\u002F_nuxt\u002Fstatic\u002F1654326224",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fawflister.github.io\u002F",assetsPath:"\u002Fawflister.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
