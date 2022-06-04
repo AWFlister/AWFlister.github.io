557c854ccf096ab800d03fbb5df10eb7c97a0703
Aryasakti Wirasena <aryasaktiwirasena@gmail.com>
2022-06-04 13:54:02 +0700

    Updates

--- dist/_nuxt/60961ae.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{278:function(e,n,l){"use strict";l.r(n);var t={name:"IndexPage"},o=l(61),component=Object(o.a)(t,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("p",[e._v("Hello World, please be nice to me")])}),[],!1,null,null,null);n.default=component.exports}}]);
--- pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <p>Hello World, please be nice to me</p>
-</template>
-
-<script>
-export default {
-  name: 'IndexPage'
-}
-</script>
